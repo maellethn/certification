b6e7c564a8a1fe1883002ca1f4629482b3ec7a36
mthinon <mthinon@jems-group.com>
2024-08-29 11:39:01 +0200

    add role security to acces admin functionality

--- src/Controller/Front/HomepageController.php
+++ src/Controller/Front/HomepageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Front;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,7 +11,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_front_homepage')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('front/homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
